0dc6e71a08c11b2cad770acf7d45a9d41d4003c0
Berk <Berk@DESKTOP-F9T3H6C>
2022-02-28 21:14:43 +0300

    Add project files.

---
+++ UsersManagement.API/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace UsersManagement.API.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+    }
+}
